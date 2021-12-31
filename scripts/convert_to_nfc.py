be315bdbca3d8429e0b5eefe63704102442c8f40
fhardison <fhardison@gmail.com>
2021-12-31 07:44:33 -0500

    more work on conversion script and adding section 4

--- scripts/convert_to_nfc.py
+++ scripts/convert_to_nfc.py
@@ -1,13 +1,20 @@
 import glob
 from greek_normalisation.utils import nfc
-DIR_PATH = '../text/*.txt'
+import os.path
+import subprocess
+import sys
+
+
+DIR_PATH = os.path.join(os.path.dirname(__file__), '..', 'text')
 
 
 REPLACEMENTS = [
     ('\u1FBF', '\u2019'),
     ('\r\n', '\n'),
+    ('\u0387', '\u00B7'),
     ('\u0387', '\u00B7')
 ]
+
 
 def process(ifile):
     print(ifile)
@@ -20,7 +27,29 @@ def process(ifile):
     text = nfc(converted)
     with open(ifile, 'w', encoding="UTF-8") as f:
         f.write(text)
-        
-        
-for f in glob.glob(DIR_PATH):
-    process(f)
+    convert_line_endings(ifile)
+
+
+def convert_line_endings(ifile):    
+    text = ''
+    with open(ifile, 'rb') as f:
+        text = f.read()                
+    text = text.replace(b'\r\n', b'\n')
+    with open(ifile, 'wb') as f:
+        f.write(text)
+
+
+FILES = list(glob.glob(DIR_PATH + '\\*.txt'))
+
+
+def process_files():
+    for f in FILES:
+        process(f)
+
+
+if len(sys.argv) > 1 and '-v' in sys.argv[1]:
+    args = ['validate-text', os.path.join(os.path.dirname(__file__),'..', 'text-validator.toml')]
+    args.extend(FILES)
+    subprocess.run(args)
+else:
+    process_files()
