139e8d3fdf607dffcc95b859652f91e2b2394b46
Fletcher Hardison <fhardison@@gmail.com>
2022-12-02 07:10:18 -0500

    adding chapter 14

---
+++ scripts/validate.py
@@ -0,0 +1,15 @@
+from text_validator.main import validate
+from pathlib import Path
+
+TEXTS = Path('../text')
+
+files = list(TEXTS.glob('**/*.txt'))
+
+nums = [f"{num:02d}" for num in range(1,15)]
+
+check = ["26"] + nums
+
+VALIDATED = [Path(f'../text/{x}-on-the-incarnation.txt') for x in check]
+
+validate(Path('../text-validator.toml'), VALIDATED)
+# ’
