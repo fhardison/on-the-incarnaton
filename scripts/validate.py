ec761257f22dcb7099855669951209d53738084c
Fletcher Hardison <fhardison@@gmail.com>
2022-12-03 20:29:11 -0500

    finished text; still needs validating

--- scripts/validate.py
+++ scripts/validate.py
@@ -1,13 +1,18 @@
 from text_validator.main import validate
 from pathlib import Path
+from greek_normalisation.utils import nfc
 
 TEXTS = Path('../text')
 
 files = list(TEXTS.glob('**/*.txt'))
+# for f in files:
+#     out = f.read_text().strip()
+#     f.write_text(out + "\n")
+# exit()
 
-nums = [f"{num:02d}" for num in range(1,25)]
+nums = [f"{num:02d}" for num in range(1,31)]
 
-check = ["26"] + nums
+check = nums
 
 VALIDATED = [Path(f'../text/{x}-on-the-incarnation.txt') for x in check]
 
