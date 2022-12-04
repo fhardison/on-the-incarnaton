0839b53d5395fe63ea4ae8e54fa2aecd7892bc09
Fletcher Hardison <fhardison@@gmail.com>
2022-12-03 21:25:05 -0500

    building site

---
+++ scripts/build_site.py
@@ -0,0 +1,21 @@
+
+from pathlib import Path
+import markdown2
+
+TEXTS = Path('../text')
+DOCS = Path('../docs')
+
+files = list(TEXTS.glob('**/*.txt'))
+
+buffer = []
+for f in files:
+    out = f.read_text().strip()
+    number = out.split(' ', maxsplit=1)[0].split('.', maxsplit=1)[0]
+    buffer.append((int(number), '# Κεφάλαιον '+ number + '\n '+ out))
+
+
+output = markdown2.markdown('\n'.join([x[1] for x in sorted(buffer, key=lambda x: x[0])]))
+
+template = Path('./rouse.html').read_text().replace('$body$', output)
+
+(DOCS / Path("on-the-incarnation.html")).write_text(template)
