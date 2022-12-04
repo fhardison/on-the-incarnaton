35cb731696310770fb5f9f3f7c4e488869a42eea
Fletcher Hardison <fhardison@@gmail.com>
2022-12-03 19:37:04 -0500

    adding 23

--- scripts/validate.py
+++ scripts/validate.py
@@ -5,7 +5,7 @@ TEXTS = Path('../text')
 
 files = list(TEXTS.glob('**/*.txt'))
 
-nums = [f"{num:02d}" for num in range(1,23)]
+nums = [f"{num:02d}" for num in range(1,24)]
 
 check = ["26"] + nums
 
